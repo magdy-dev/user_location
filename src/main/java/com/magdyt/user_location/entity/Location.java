7acbf32ce1fe2aa4805c5d1440cbe1dc09ba9e69
mady shenoda <markobosh5@gmail.com>
2022-04-13 10:49:33 +0300

    start git user app

---
+++ src/main/java/com/magdyt/user_location/entity/Location.java
@@ -0,0 +1,4 @@
+package com.magdyt.user_location.entity;
+
+public class Location {
+}
---
+++ src/main/java/com/magdyt/user_location/entity/User.java
@@ -0,0 +1,11 @@
+package com.magdyt.user_location.entity;
+
+public class User {
+
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Location location;
+    private  String email;
+}
